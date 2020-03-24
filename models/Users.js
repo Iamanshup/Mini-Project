cf38ed8e2f063ecedc9374b6837370759ee55c93
iamanshup <raj.anshu66@gmail.com>
2020-03-24 16:17:41 +0530

    remove boat number for register

--- models/Users.js
+++ models/Users.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
 var uniqueValidator = require("mongoose-unique-validator");
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  boatNumber: {
-    type: String,
-    required: true
-  }
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+	// boatNumber: {
+	//   type: String,
+	//   required: true
+	// }
 });
-userSchema.methods.validPassword = function (password) {
-  return this.password === password;
+userSchema.methods.validPassword = function(password) {
+	return this.password === password;
 };
 userSchema.plugin(uniqueValidator);
 const userModel = mongoose.model("Users", userSchema);
