b49260d92c4b1bcaf715d0b198f308d74c6d0a00
iamanshup <raj.anshu66@gmail.com>
2020-06-16 15:15:59 +0530

    added comments

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const Item = mongoose.model("Items");
-const { ensureAuthenticated } = require("../helpers/auth");
+const {
+  ensureAuthenticated
+} = require("../helpers/auth");
+
 router.route("/").get((req, res) => {
   res.render("index/welcome");
 });
+
 router.route("/about").get((req, res) => {
   res.render("index/about");
 });
 
+// Show all auctions(items) a user has created on the dashboard page
 router.route("/dashboard").get(ensureAuthenticated, (req, res) => {
-  Item.find({ user: req.user.id }).then(items => {
+  Item.find({
+    user: req.user.id
+  }).then(items => {
     res.render("index/dashboard", {
       items: items
     });
@@ -19,6 +26,6 @@ router.route("/dashboard").get(ensureAuthenticated, (req, res) => {
 });
 
 router.route("/about").get((req, res) => {
-  res.send("index/about");
+  res.send("index/about"); // render is same as send(render = send)
 });
 module.exports = router;
