1ca795f96bc0afd093a09fdf2a32c1bc5d6be131
iamanshup <raj.anshu66@gmail.com>
2020-06-16 15:40:39 +0530

    added comments

--- routes/items.js
+++ routes/items.js
@@ -3,14 +3,18 @@ const mongoose = require("mongoose");
 const router = express();
 const Item = mongoose.model("Items");
 const Boat = mongoose.model("Boats");
-const { ensureAuthenticated } = require("../helpers/auth");
+const {
+	ensureAuthenticated
+} = require("../helpers/auth");
 
+// Code to show add item form
 router.route("/add").get(ensureAuthenticated, (req, res) => {
 	res.render("items/add");
 });
 
 router
 	.route("/")
+	// Code to show all the auctions
 	.get((req, res) => {
 		Item.find().then((items) => {
 			res.render("items/index", {
@@ -19,6 +23,7 @@ router
 		});
 	})
 
+	// Code to create an auction and store it in database
 	.post(ensureAuthenticated, (req, res) => {
 		// x contains the extracted bid date from request in the Date object format
 		let x = new Date(req.body.bid_date);
@@ -29,9 +34,12 @@ router
 		}
 
 		let boat_number = req.body.boat_number;
+
+		// If the boat_number given by user does not exist in the Boat database
+		// then show an error else proceed to create the auction
 		Boat.find({
-			boat_number: boat_number,
-		})
+				boat_number: boat_number,
+			})
 			.then((boat) => {
 				// console.log(boat);
 				if (boat.length == 0) {
@@ -73,6 +81,7 @@ router
 			.catch((err) => console.log(err));
 	});
 
+// Code to show the description of an auction
 router.route("/show/:id").get((req, res) => {
 	Item.findById(req.params.id)
 		.populate("user") // Populate replaces the id with all the other information of the user
@@ -100,10 +109,12 @@ router.route("/show/:id").get((req, res) => {
 //   });
 // });
 
+
+// Code to show all my auctions
 router.route("/my").get(ensureAuthenticated, (req, res) => {
 	Item.find({
-		user: req.user.id,
-	})
+			user: req.user.id,
+		})
 		.sort({
 			bid_time: "desc",
 		})
@@ -114,21 +125,29 @@ router.route("/my").get(ensureAuthenticated, (req, res) => {
 		});
 });
 
+// Code to aad bid on an auction
 router.route("/addbid/:id").post(ensureAuthenticated, (req, res) => {
 	Item.findById(req.params.id).then((item) => {
+
+		// If bidding amount is not given show an error
 		if (!req.body.amount) {
 			req.flash("error_msg", "Please provide a valid amount.");
 			return res.redirect(`/items/show/${req.params.id}`);
 		}
+
+		// If amount of fish needed is not given show an error
 		if (!req.body.fish_amount) {
 			req.flash("error_msg", "Please provide a valid amount of fish.");
 			return res.redirect(`/items/show/${req.params.id}`);
 		}
+
+		// Create a bid object
 		const newBid = {
 			amount: req.body.amount,
 			fish_amount: req.body.fish_amount,
 			user: req.user.id,
 		};
+
 		item.bids.unshift(newBid); // append the new bid in the bids array of the item
 		item.save();
 		req.flash("success_msg", "Your Bid was successfully placed");
@@ -136,10 +155,11 @@ router.route("/addbid/:id").post(ensureAuthenticated, (req, res) => {
 	});
 });
 
+// Code to show all auctions created by a particular user
 router.get("/user/:userId", (req, res) => {
 	Item.find({
-		user: req.params.userId,
-	})
+			user: req.params.userId,
+		})
 		.populate("user")
 		.then((items) => {
 			res.render("items/index", {
@@ -148,29 +168,26 @@ router.get("/user/:userId", (req, res) => {
 		});
 });
 
+// Code to delete an auction
+// A user can delete only the auctions he himself created
 router.route("/:id").delete(ensureAuthenticated, (req, res) => {
-	Item.remove({
-		_id: req.params.id,
-	}).then(() => {
-		req.flash("success_msg", "Item Successfully deleted.");
-		res.redirect("/dashboard");
-	});
+	Item.findById(req.params.id).then(item => {
+
+		// If some other user is trying to delete current user's auction
+		// show an error message
+		if (req.user._id.toString() != item.user.toString()) {
+			req.flash("error_msg", "You are not authorized to do that.");
+			return res.redirect(`/dashboard`);
+		}
+
+		// else delete the auction
+		Item.remove({
+			_id: req.params.id,
+		}).then(() => {
+			req.flash("success_msg", "Item Successfully deleted.");
+			res.redirect("/dashboard");
+		});
+	})
 });
-// .put(ensureAuthenticated, (req, res) => {
-// 	Item.findById(req.params.id).then(item => {
-// 		let x = new Date(req.body.bid_date);
 
-// 		(item.name = req.body.name),
-// 		(item.image = req.body.image),
-// 		(item.description = req.body.description),
-// 		(item.user = req.user.id),
-// 		(item.status = req.body.status),
-// 		(item.bid_time = x);
-
-// 		item.save().then(item => {
-// 			req.flash("success_msg", "Changes saved successfully");
-// 			res.redirect(`/items/show/${item._id}`);
-// 		});
-// 	});
-// });
 module.exports = router;
